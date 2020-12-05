252482625310223bf374bc0b184bb1a0ae32cc5f
sundar <sundarban007@gmail.com>
2020-12-05 11:29:18 +1100

    migration script changed

--- src/migrations/20201205102449_create_customers_table.js
+++ src/migrations/20201205102449_create_customers_table.js
@@ -10,8 +10,8 @@ exports.up = function (knex) {
     table.string('first_name').notNullable();
     table.string('middle_name').nullable();
     table.string('last_name').notNullable();
-    table.string('email').notNullable().unique('email');
-    table.string('phone').notNullable().unique('phone');
+    table.string('email').notNullable().unique('cus_email');
+    table.string('phone').notNullable().unique('cus_phone');
     table.string('password').notNullable();
     table.string('nationality').nullable();
     table.string('profile_image').nullable();
@@ -19,7 +19,7 @@ exports.up = function (knex) {
     table.double('wallet_amount', 10, 2).default(0);
     table.double('total_purchase', 10, 2).default(0);
     table.bool('is_verified').default(0);
-    table.string('status');
+    table.string('status').default('invited');
     table.string('remember_token').nullable();
     table.string('otp_code').nullable();
     table.timestamp('created_at').defaultTo(knex.fn.now());
