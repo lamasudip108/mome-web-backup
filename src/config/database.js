9ca123163cd35b8c6addf8a2089cb3574ff141b8
Krishna Timilsina <krishna.timilsina@citytech.global>
2020-12-14 17:52:56 +0545

    improvements

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 export default {
-  client: process.env.DB_CLIENT,
+  client: 'mysql',
   connection: {
     host: process.env.DB_HOST || '127.0.0.1',
     user: process.env.DB_USER,
