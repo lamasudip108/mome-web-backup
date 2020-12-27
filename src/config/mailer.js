34888706a7c0b26520c5dc7ee3fbd54c659c9bfa
sundar <sundarban007@gmail.com>
2020-12-27 23:16:26 +1100

    gmail setup

--- src/config/mailer.js
+++ src/config/mailer.js
@@ -13,12 +13,13 @@ const senderEmail = '"Mome" <noreply@mome.com>';
 
 function setup() {
   return nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
+    service: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
+    secure: process.env.EMAIL_SECURE,
     auth: {
       user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
+      pass: process.env.EMAIL_PASS
+    }
   });
 }
 
