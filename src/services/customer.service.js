17cef48deb14c60566b404b3b17db8bb49f4130a
Krishna Timilsina <krishna.timilsina@citytech.global>
2021-01-23 15:49:08 +0545

    improvements

--- src/services/customer.service.js
+++ src/services/customer.service.js
@@ -23,7 +23,7 @@ export function getAll() {
  */
 export function getOne(criteria) {
   return new Customer(criteria)
-    .fetch({ require: false })
+    .fetch({ require: true })
     .then((user) => user)
     .catch(Customer.NotFoundError, () => {
       throw Boom.notFound('Customer not found.');
